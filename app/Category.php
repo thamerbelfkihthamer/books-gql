b3311849b3acdf75cd43c45d37e1507157cd7ed8
thamerbelfkihthamer <thamer.belfkih@gmail.com>
2020-03-24 11:29:46 +0100

    add caterory & book models, seeders, factories, migrations

---
+++ app/Category.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    protected $guarded = [];
+
+    public function books()
+    {
+        return $this->hasMany(Book::class);
+    }
+
+}
---
+++ database/factories/BookFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Book;
+use Faker\Generator as Faker;
+
+$factory->define(Book::class, function (Faker $faker) {
+    return [
+        'title' => $faker->title,
+        'author' => $faker->userName,
+        'description' => $faker->text,
+        'image' => $faker->image('public/storage/images',640,480, null, false),
+        'category_id' => factory(\App\Category::class)->create()
+    ];
+});
